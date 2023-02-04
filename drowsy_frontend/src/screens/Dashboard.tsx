192912eac7382ed6eb1187d0df4fc09a1319950c
kestertan <kestert@andrew.cmu.edu>
2023-02-03 20:38:56 -0500

    frontend changes

--- drowsy_frontend/src/screens/Dashboard.tsx
+++ drowsy_frontend/src/screens/Dashboard.tsx
@@ -10,114 +10,15 @@ import React, { useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Dashboard: React.FC = ({ navigation }: any) => {
-  const [Point, setPoint] = useState(120);
-  const [Streak, setStreak] = useState(5);
-  const [TotalStreak, setTotalStreak] = useState(10);
-  const [Succeed, setSucceed] = useState("gold");
-  const image = { uri: 'https://cdn-icons-png.flaticon.com/512/1213/1213709.png' }
+  
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={styles.container}>
-          <Text style={styles.bigTitle}>Rewards</Text>
+          <Text style={styles.bigTitle}>Dashboard</Text>
         </View>
-        {/*<Card style={[styles.card, { backgroundColor: "#ace4f5" }]}>
-          <CustomButton
-            mode="outlined"
-            color="#dfc4fb"
-            style={styles.alarmButton}
-            onPress={() => navigation.navigate("Alarms")}
-          >
-            <Text style={styles.setAlarmNow}>SET ALARM NOW</Text>
-          </CustomButton>
-        </Card>*/}
-        <Card style={styles.card}>
-          <Text style={styles.label}>Total Streak: {TotalStreak} 🔥 </Text>
-          <View style={styles.container2}>
-            <Text
-              style={[
-                styles.circle,
-                { backgroundColor: Streak >= 1 ? Succeed : "silver" },
-              ]}
-            >
-              Mon
-            </Text>
-            <Text
-              style={[
-                styles.circle,
-                { backgroundColor: Streak >= 2 ? Succeed : "silver" },
-              ]}
-            >
-              Tue
-            </Text>
-            <Text
-              style={[
-                styles.circle,
-                { backgroundColor: Streak >= 3 ? Succeed : "silver" },
-              ]}
-            >
-              Wed
-            </Text>
-            <Text
-              style={[
-                styles.circle,
-                { backgroundColor: Streak >= 4 ? Succeed : "silver" },
-              ]}
-            >
-              {" "}
-              Thur{" "}
-            </Text>
-            <Text
-              style={[
-                styles.circle,
-                { backgroundColor: Streak >= 5 ? Succeed : "silver" },
-              ]}
-            >
-              Fri
-            </Text>
-            <Text
-              style={[
-                styles.circle,
-                { backgroundColor: Streak >= 6 ? Succeed : "silver" },
-              ]}
-            >
-              Sat
-            </Text>
-            <Text
-              style={[
-                styles.circle,
-                { backgroundColor: Streak >= 7 ? Succeed : "silver" },
-              ]}
-            >
-              Sun{" "}
-            </Text>
-          </View>
-        </Card>
-        {/* Card for the streaks */}
-        <Card style={styles.card}>
-          <Text style={styles.label}>Coins Collected: {Point}🪙 </Text>
-        </Card>
-        <Card style={styles.card}>
-          <Text style={styles.text}>Claim Crypotcurrency </Text>
-          <ImageBackground
-            source={image}
-            resizeMode="cover"
-            style={styles.giftImage}
-          ></ImageBackground>
-        </Card>
-
-        <Card style={styles.card}>
-          <Text style={styles.text}>Share With Friends 😄 </Text>
-          <Button
-            mode="contained"
-            style={styles.referButton}
-            onPress={() => undefined}
-            color="#fff"
-          >
-                        Refer a Friend
-          </Button>
-        </Card>
+  
       </ScrollView>
     </SafeAreaView>
   );
@@ -134,12 +35,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     padding: 18,
-  },
-  container2: {
-    paddingVertical: 12,
-    flex: 3,
-    flexDirection: "row",
-    justifyContent: "space-around",
   },
   box: {
     width: 50,
@@ -165,18 +60,6 @@ const styles = StyleSheet.create({
     alignSelf: "flex-start",
     marginHorizontal: "1%",
     marginBottom: 6,
-    minWidth: "100%",
-    textAlign: "center",
-  },
-  referButton: {
-    color: "white",
-  },
-  alarmButton: {
-    height: 80,
-    borderRadius: 40,
-    backgroundColor: "oldlace",
-    alignSelf: "flex-start",
-    paddingTop: 20,
     minWidth: "100%",
     textAlign: "center",
   },
@@ -221,13 +104,7 @@ const styles = StyleSheet.create({
     height: 200,
     width: 200,
         marginLeft: 30,
-  },
-  alarmCard: {
-    fontSize: 20,
-  },
-  setAlarmNow: {
-    fontSize: 20,
-  },
+  }
 });
 
 export default Dashboard;
--- drowsy_frontend/src/screens/Profile.tsx
+++ drowsy_frontend/src/screens/Profile.tsx
@@ -12,10 +12,10 @@ const Profile: React.FC = ({ navigation }) => {
             <Avatar.Image
               size={180}
               source={{
-                uri: "https://media-exp1.licdn.com/dms/image/C4D03AQFPjoSN1SeT-w/profile-displayphoto-shrink_800_800/0/1641088977909?e=2147483647&v=beta&t=z9RIZbhvK24X2jdAUEBZ18j0bnH50_w3XqWsgPTnSV4",
+                uri: "https://freeimage.host/i/H18T2CN",
               }}
             />
-            <Text style={styles.bigTitle}>Tjun Jet</Text>
+            <Text style={styles.bigTitle}>Kester</Text>
           </View>
           <View style={[styles.container, styles.inputContainer]}>
             <Button mode="contained">Update Email</Button>
@@ -38,16 +38,6 @@ const Profile: React.FC = ({ navigation }) => {
                     <DataTable.Title>Sex</DataTable.Title>
                     <DataTable.Cell>M</DataTable.Cell>
                   </DataTable.Row>
-                  <DataTable.Row>
-                    <DataTable.Title>Medical History</DataTable.Title>
-                    <DataTable.Cell> Type 2 Diabetes </DataTable.Cell>
-                  </DataTable.Row>
-                  <DataTable.Row>
-                    <DataTable.Title>{""}</DataTable.Title>
-                    <DataTable.Cell>
-                      Ulnar Collateral Ligament Tear
-                    </DataTable.Cell>
-                  </DataTable.Row>
                 </DataTable>
               </View>
             ) : (
@@ -56,12 +46,9 @@ const Profile: React.FC = ({ navigation }) => {
                 onPress={() => setHealthInfoLoaded(true)}
                 style={{ marginTop: 20 }}
               >
-                Connect to your Health Provider
+                Create your profile
               </Button>
             )}
-            <Button mode="contained" style={{ marginTop: 20 }}>
-              Update Health Info
-            </Button>
           </View>
         </View>
       </ScrollView>
